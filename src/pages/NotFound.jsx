c4fb96024768f1cf7236a738f40ec1a5deac95ef
Steven Reales <stevenreales@gmail.com>
2022-02-26 16:09:38 -0500

    Styled Not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,10 @@
 function NotFound() {
     return ( 
-        <div>
-            Not founf 404.
-        </div>
+        <main className="text-center">
+            <h1 className="display-1">404</h1>
+            <p className="lead">La página a la que está tratando de ingresar, no existe.</p>
+            <button className="btn btn-dark">Ir al inicio</button>
+        </main>
      );
 }
 
